b2163681206c1dd14c4741e0cb13d91cba197fa3
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-06 18:29:35 +0900

    src

--- src/practice.ts
+++ src/practice.ts
@@ -1,12 +1,14 @@
 let  count = 0;
 count += 1;
-conut = '문자열';
 
 const message : string = 'hello world';
 const done: boolean = false;
 const number: number[] = [1,2,3];
 const messages: string[] = ['hello', 'world'];
 
-messages.push(1);
-
 let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+
+let color: 'red' | 'orange' | 'yellow' ='red';
+color = 'orange';

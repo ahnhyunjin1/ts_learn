289810f782064f7248f8ca19f8a230855e979cc3
ahnhyunjin1 <a01020813765@gmail.com>
2022-01-06 18:26:27 +0900

    src

--- src/practice.ts
+++ src/practice.ts
@@ -1,1 +1,12 @@
-const message: string = 'hello world';
+let  count = 0;
+count += 1;
+conut = '문자열';
+
+const message : string = 'hello world';
+const done: boolean = false;
+const number: number[] = [1,2,3];
+const messages: string[] = ['hello', 'world'];
+
+messages.push(1);
+
+let mightBeUndefined: string | undefined = undefined;
